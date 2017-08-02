5a31146b797f6ea836bd6f331c485ba824660719
qiyun004 <1809275454@qq.com>
2017-08-02 17:02:51 +0800

    qiyun004
    调整了Application.class.php中方法的顺序

--- KUAIXUEPHP/Lib/Core/Application.class.php
+++ KUAIXUEPHP/Lib/Core/Application.class.php
@@ -12,46 +12,6 @@ final class Application{
 		self::_app_run();
 	}
 
-	//实例化应用控制器
-	private static function _app_run(){
-		$c = isset($_GET['VAR_CONTROLLER'])?$_GET['VAR_CONTROLLER']:"Index";
-		$a = isset($_GET['VAR_ACTION'])?$_GET['VAR_ACTION']:"index";
-
-		$c .= 'Controller';
-		$obj = new $c();
-		$obj->$a(); 
-	}
-
-	//创建默认控制器
-	private static function _create_demo(){
-		$path = APP_CONTROLLER_PATH . '/IndexController.class.php';
-		$str = <<<str
-<?php
-class IndexController extends Controller{
-	public function index(){
-		echo "ok";
-	}
-}
-?>
-str;
-		is_file($path) || file_put_contents($path, $str);
-	}
-
-	//自动载入
-	private static function _autoload($className){
-		//echo $className;//IndexController
-		include APP_CONTROLLER_PATH.'/'.$className.'.class.php';
-	}
-	//设置外部路径
-	private static function _set_url(){
-		$path = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
-		$path = str_replace('\\', '/', $path);
-		define('__APP__',$path);//http://localhost/KUAIXUEAPP/index.php
-		define('__ROOT__',dirname($path));
-		
-		define('__TPL__',__ROOT__.'/'.APP_NAME.'/Tpl');
-		define('__PUBLIC__',__TPL__.'/Public');
-	}
 	//初始化框架
 	private static function _init(){
 		//加载配置项
@@ -78,5 +38,49 @@ str;
 		C('SESSION_AUTO_START') && session_start();
 
 	}
+	//设置外部路径
+	private static function _set_url(){
+		$path = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
+		$path = str_replace('\\', '/', $path);
+		define('__APP__',$path);//http://localhost/KUAIXUEAPP/index.php
+		define('__ROOT__',dirname($path));
+		
+		define('__TPL__',__ROOT__.'/'.APP_NAME.'/Tpl');
+		define('__PUBLIC__',__TPL__.'/Public');
+	}
+	//自动载入
+	private static function _autoload($className){
+		//echo $className;//IndexController
+		include APP_CONTROLLER_PATH.'/'.$className.'.class.php';
+	}
+	//创建默认控制器
+	private static function _create_demo(){
+		$path = APP_CONTROLLER_PATH . '/IndexController.class.php';
+		$str = <<<str
+<?php
+class IndexController extends Controller{
+	public function index(){
+		echo "ok";
+	}
+}
+?>
+str;
+		is_file($path) || file_put_contents($path, $str);
+	}
+	//实例化应用控制器
+	private static function _app_run(){
+		$c = isset($_GET['VAR_CONTROLLER'])?$_GET['VAR_CONTROLLER']:"Index";
+		$a = isset($_GET['VAR_ACTION'])?$_GET['VAR_ACTION']:"index";
+
+		$c .= 'Controller';
+		$obj = new $c();
+		$obj->$a(); 
+	}
+
+	
+
+	
+	
+	
 }
 ?>
